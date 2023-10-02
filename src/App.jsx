5fbc088cb8af44b1d8b6b055236860456df4ad00
Richard Jeremy Githuba <githuba9520@gmail.com>
2023-10-02 21:16:59 +0300

    nested my navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,24 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
-import Navbar from "./Components/Navbar/Navbar";
 import Vans from "./Components/Vans/Vans";
-import Footer from "./Components/Footer/Footer";
 import "../server";
 import VansDetails from "./Components/Vans/VansDetails";
+import Layout from "./Components/Layout";
+
+
 
 function App() {
   return (
     <div className="app-container">
-      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VansDetails />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VansDetails />} />
+        </Route>
       </Routes>
-      <Footer />
     </div>
   );
 }
---
+++ src/Components/Layout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar/Navbar";
+import Footer from "./Footer/Footer";
+import "../app.css"
+
+function Layout() {
+  return (
+    <div className="layout-container">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout;
