74cc9c93b114d17c77ac121dfb7143f8d211c732
Richard Jeremy Githuba <githuba9520@gmail.com>
2023-10-04 10:37:40 +0300

    fix: import fix

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer/Footer";
-import "../app.css"
+import "../App.css"
 
 function Layout() {
   return (
